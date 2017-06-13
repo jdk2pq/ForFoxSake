53b8180a77d5ba5a8736cd1d66966c7e7345833c
Jake Kenneally <jdk2pq@ccri.com>
2017-06-13 00:10:19 -0400

    Oh, for Fox sake v. 0.0.2

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const request = require('request');
+const moment = require('moment');
+const _ = require('lodash');
+
+const app = express();
+
+const inaugurationDay = moment('2017-01-20');
+
+
+function getUrl(searchString) {
+      	const today = new Date();
+        let year = today.getFullYear(),
+  			day = today.getDate(),
+  			month = today.getMonth(),
+  			todayString = '';
+
+  		month = month + 1;
+      if (month.toString().length === 1) {
+        month = '0' + month;
+      } else {
+        month = month.toString();
+      }
+
+  		if (day.toString().length === 1) {
+  			day = '0' + day;
+  		} else {
+        day = day.toString();
+      }
+
+  		if (searchString === 'obama') {
+  			year = year - 8;
+  		}
+  		todayString = year + '-' + month + '-' + day;
+  		return 'http://api.foxnews.com/v1/content/search?q=' + searchString +
+        '&fields=date,description,title,url,image,type,taxonomy&section.path=fnc&min_date=' +
+        todayString + '&max_date=' + todayString + '&start=0&cb=201769210&callback=?';
+}
+
+function getHeadlineList(body) {
+  return _.map(_.sortBy(_.uniqBy(JSON.parse(body).response.docs, 'title'), function (a) { return moment(a.date).valueOf(); }), function(article) {
+      let link = article.url;
+      if (link.length === 1) {
+        link = link[0];
+      }
+      return {url: link, title: article.title};
+  });
+}
+
+function getDateString(obama) {
+  let today = moment();
+  if (obama) {
+    today = today.subtract(8, 'years');
+  }
+  return today.format('dddd MMMM DD, YYYY');
+}
+
+let daysSinceStartOfPresidency = moment().diff(inaugurationDay, 'days');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,8 +65,22 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+	request(getUrl('obama'), function (errorO, responseO, bodyO) {
+	    let trumpDate = getDateString(false),
+	        obamaDate = getDateString(true),
+	        obamaHeadlines = getHeadlineList(bodyO);
+	    request(getUrl('trump'), function (errorT, responseT, bodyT) {
+	        let listTrump = getHeadlineList(bodyT);
+	        res.render('pages/index', {
+			  	daysSinceStartOfPresidency: daysSinceStartOfPresidency,
+			  	trumpDate: trumpDate,
+			  	obamaDate: obamaDate,
+			  	trumpHeadlines: listTrump,
+			  	obamaHeadlines: obamaHeadlines
+			});
+      	});
+  	});
 });
 
 app.listen(app.get('port'), function() {
